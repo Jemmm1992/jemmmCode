b5b58499951d5d523fb501c62bd2bfa2d94258cc
徐志坚 <xu_zj@Ctrip.com>
2017-06-29 18:27:22 +0800

    添加数组转化成链表的方法和打印链表的方法

--- src/main/java/com/jemmm/utils/Utils.java
+++ src/main/java/com/jemmm/utils/Utils.java
@@ -1,5 +1,7 @@
 package com.jemmm.utils;
 
+import com.jemmm.leetcode.common.ListNode;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +9,9 @@ import java.util.List;
  * Created by BIG-JIAN on 2017/6/18.
  */
 public class Utils {
+    /**
+     * 输出数组
+     */
     public static void printf(ArrayList<ArrayList<Integer>> lists){
         for (List<Integer> list: lists) {
             for (Integer i :list) {
@@ -16,6 +21,7 @@ public class Utils {
         }
     }
 
+
     public static void printf(int [] a){
             for (Integer i :a) {
                 System.out.printf("%5d",i);
@@ -23,9 +29,45 @@ public class Utils {
             System.out.println();
     }
 
+
+    /**
+     * 交换数组中的两个数
+     * @param a
+     * @param i
+     * @param j
+     */
     public static void swap(int[] a,int i,int j){
         int tmp = a[i];
         a[i] = a[j];
         a[j] = tmp;
+    }
+
+    /**
+     * 直接将一个整形数组转化成对应的链表
+     * @param a
+     * @return
+     */
+    public static ListNode arrayToListNode(int [] a){
+        ListNode head= new ListNode(0);
+        ListNode n = head;
+        for (Integer o:a) {
+            ListNode node = new ListNode(o);
+            n.next = node;
+            n = n.next;
+        }
+        return head.next;
+    }
+
+    /**
+     * 打印链表
+     * @param head
+     */
+    public static void printfListNode(ListNode head){
+        ListNode n = head;
+        while(n != null){
+            System.out.print(n.val + " -> ");
+            n = n.next;
+        }
+        System.out.println("null");
     }
 }
