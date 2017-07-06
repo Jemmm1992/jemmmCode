a4e5766ab3cc2f3918b4fe3dc20aee2be6abda3a
xu_zj <344345929@qq.com>
2017-07-06 22:51:02 +0800

    LargestRectangleInHistogram

---
+++ src/main/java/com/jemmm/leetcode/greedy/MaximalRectangle.java
@@ -0,0 +1,12 @@
+package com.jemmm.leetcode.greedy;
+
+/**
+ * Created by BIG-JIAN on 2017/7/6.
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing all ones and return its area.
+ */
+public class MaximalRectangle {
+    public int maximalRectangle(char[][] matrix) {
+
+        return 0;
+    }
+}
